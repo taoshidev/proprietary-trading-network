f7466ff5ff8a4a710abaec3afb1a3ed2bcee257f
taoshidev1 <myasavolian90@gmail.com>
2024-03-22 14:48:37 -0700

    set weights version
    update version for weights version

--- template/__init__.py
+++ template/__init__.py
@@ -3,7 +3,7 @@
 # developer: Taoshidev
 # Copyright © 2024 Taoshi Inc
 
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 version_split = __version__.split(".")
 __spec_version__ = (1000 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
 
