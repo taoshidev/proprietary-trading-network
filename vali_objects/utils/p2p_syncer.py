1c5b773783ffe8292947d1127d26b2b42e5d5d73
sli-tao <sli@taoshi.io>
2024-07-03 12:43:18 -0700

    adjust interval time

--- vali_objects/utils/p2p_syncer.py
+++ vali_objects/utils/p2p_syncer.py
@@ -144,7 +144,7 @@ class P2PSyncer(ValidatorSyncBase):
         if self.is_testnet:
             datetime_now = TimeUtil.generate_start_timestamp(0)  # UTC
             # every hour in testnet
-            if not (8 < datetime_now.minute < 20):
+            if not (47 < datetime_now.minute < 57):
                 return
         else:
             now_ms = TimeUtil.now_in_millis()
@@ -155,7 +155,7 @@ class P2PSyncer(ValidatorSyncBase):
             # Check if we are between 7:09 AM and 7:19 AM UTC
             datetime_now = TimeUtil.generate_start_timestamp(0)  # UTC
             # Temp change time to 21:00 UTC so we can see the effects in shadow mode ASAP
-            if not (datetime_now.hour == 21 and (37 < datetime_now.minute < 47)):
+            if not (datetime_now.hour == 21 and (8 < datetime_now.minute < 20)):
                 return
 
         try:
