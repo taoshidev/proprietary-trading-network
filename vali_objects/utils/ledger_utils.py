597749ecfedbd9d84a29e173b1012865f770d543
Thomas Dougherty <tdougherty@taoshi.io>
2024-11-25 13:11:18 -0500

    Updating for new abnormality update and mean drawdown

--- vali_objects/utils/ledger_utils.py
+++ vali_objects/utils/ledger_utils.py
@@ -242,6 +242,22 @@ class LedgerUtils:
         return 1
 
     @staticmethod
+    def mean_drawdown(checkpoints: list[PerfCheckpoint]) -> float:
+        """
+        Args:
+            checkpoints: list[PerfCheckpoint] - the list of checkpoints
+        """
+        if len(checkpoints) == 0:
+            return 0
+
+        # Compute the drawdown of the checkpoints
+        drawdowns = [checkpoint.mdd for checkpoint in checkpoints]
+        effective_drawdown = np.mean(drawdowns)
+        final_drawdown = np.clip(effective_drawdown, 0, 1.0)
+
+        return final_drawdown
+
+    @staticmethod
     def approximate_drawdown(checkpoints: list[PerfCheckpoint]) -> float:
         """
         Args:
@@ -310,7 +326,7 @@ class LedgerUtils:
             return 0
 
         # recent_drawdown = LedgerUtils.recent_drawdown(checkpoints)
-        approximate_drawdown = LedgerUtils.approximate_drawdown(checkpoints)
+        approximate_drawdown = LedgerUtils.mean_drawdown(checkpoints)
 
         # effective_drawdown = LedgerUtils.effective_drawdown(approximate_drawdown)
         drawdown_penalty = LedgerUtils.mdd_augmentation(approximate_drawdown)
