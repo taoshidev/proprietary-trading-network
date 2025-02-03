0cbcf78f1201685892175d623728b85482e0bb4a
sli-tao <sli@taoshi.io>
2025-02-03 01:51:17 -0800

    slippage unit tests

--- tests/vali_tests/test_price_slippage_model.py
+++ tests/vali_tests/test_price_slippage_model.py
@@ -1,20 +1,73 @@
 from tests.vali_tests.base_objects.test_base import TestBase
+from vali_objects.utils.price_slippage_model import PriceSlippageModel
+from vali_objects.vali_config import TradePair
 
 
 class TestPriceSlippageModel(TestBase):
-
     def setUp(self):
         super().setUp()
-        pass
+        self.psm = PriceSlippageModel()
 
     # TODO: test each, also outside of traded hours
     # TODO: test each with large and small size
 
     def test_equities_slippage(self):
-        pass
+        """
+        test buy and sell order slippage
+        """
+        slippage_buy = self.psm.calculate_slippage(TradePair.AAPL, "buy", 75000, 1737655200000)
+        print(slippage_buy)
+        slippage_sell = self.psm.calculate_slippage(TradePair.AAPL, "sell", 275000, 1737655200000)
+
+    def test_equities_slippage_size(self):
+        """
+        larger size should equal larger slippage
+        smaller size should equal smaller slippage
+        """
+        slippage_less = self.psm.calculate_slippage(TradePair.AAPL, "buy", 75000, 1737655200000)
+        slippage_more = self.psm.calculate_slippage(TradePair.AAPL, "buy", 275000, 1737655200000)
+        assert slippage_less < slippage_more
+
+        slippage_less = self.psm.calculate_slippage(TradePair.AAPL, "sell", 75000, 1737655200000)
+        slippage_more = self.psm.calculate_slippage(TradePair.AAPL, "sell", 275000, 1737655200000)
+        assert slippage_less < slippage_more
 
     def test_forex_slippage(self):
-        pass
+        """
+        test buy and sell order slippage
+        """
+        slippage = self.psm.calculate_slippage(TradePair.USDJPY, "buy", 75000, 1737655200000)
+        print(slippage)
+        slippage = self.psm.calculate_slippage(TradePair.USDJPY, "sell", 275000, 1737655200000)
+
+    def test_forex_slippage_size(self):
+        """
+        larger size should equal larger slippage
+        smaller size should equal smaller slippage
+        """
+        slippage_less = self.psm.calculate_slippage(TradePair.USDJPY, "buy", 75000, 1737655200000)
+        slippage_more = self.psm.calculate_slippage(TradePair.USDJPY, "buy", 275000, 1737655200000)
+        assert slippage_less < slippage_more
+
+        slippage_less = self.psm.calculate_slippage(TradePair.USDJPY, "sell", 75000, 1737655200000)
+        slippage_more = self.psm.calculate_slippage(TradePair.USDJPY, "sell", 275000, 1737655200000)
+        assert slippage_less < slippage_more
 
     def test_crypto_slippage(self):
-        pass
+        """
+        test buy and sell order slippage
+        """
+        slippage = self.psm.calculate_slippage(TradePair.BTCUSD, "buy", 75000, 1737655200000)
+        print(slippage)
+        slippage = self.psm.calculate_slippage(TradePair.BTCUSD, "sell", 275000, 1737655200000)
+
+    def test_crypto_slippage_asset(self):
+        """
+        btcusd and ethusd have less slippage than solusd, xrpusd, and dogeusd
+        crypto slippage is constant below 50k
+        """
+        slippage_btc = self.psm.calculate_slippage(TradePair.BTCUSD, "buy", 7500, 1737655200000)
+        slippage_sol = self.psm.calculate_slippage(TradePair.SOLUSD, "sell", 7500, 1737655200000)
+        assert slippage_btc < slippage_sol
+
+    # TODO: test a buy and sell order with slippage, compare to without
--- vali_objects/utils/price_slippage_model.py
+++ vali_objects/utils/price_slippage_model.py
@@ -1,5 +1,5 @@
 import math
-from datetime import datetime, timezone, timedelta
+from datetime import datetime
 
 import numpy as np
 import pandas as pd
@@ -10,16 +10,14 @@ from vali_objects.utils.vali_utils import ValiUtils
 from vali_objects.vali_config import TradePair, ValiConfig
 
 class PriceSlippageModel:
-    def __init__(self, running_unit_tests=False):
+    def __init__(self, live_price_fetcher=None, running_unit_tests=False):
         # self.live_price_fetcher = live_price_fetcher
         self.parameters = self.read_slippage_model_parameters()
-        self.sampled_df = None
-
         self.running_unit_tests = running_unit_tests
 
-        # if self.pds is None:
-        secrets = ValiUtils.get_secrets(running_unit_tests=self.running_unit_tests)
-        live_price_fetcher = LivePriceFetcher(secrets, disable_ws=True)
+        if live_price_fetcher is None:
+            secrets = ValiUtils.get_secrets(running_unit_tests=self.running_unit_tests)
+            live_price_fetcher = LivePriceFetcher(secrets, disable_ws=True)
         self.pds = live_price_fetcher.polygon_data_service
 
     # TODO: uses model and parameters to calculate slippage percentage. in the validator we take the slippage percentage
@@ -30,6 +28,9 @@ class PriceSlippageModel:
         returns the percentage slippage of the current order.
         each asset class uses a unique model
         """
+        if size < 1000:
+            return 0  # assume 0 slippage when order size is under 1k
+
         if trade_pair.is_equities:
             slippage_percentage = self.calc_slippage_equities(trade_pair, side, size, processed_ms)
         elif trade_pair.is_forex:
@@ -52,28 +53,14 @@ class PriceSlippageModel:
 
         annualized_volatility, avg_daily_volume = self.get_bar_features(trade_pair, processed_ms)
 
-        print(annualized_volatility, avg_daily_volume)
+        # print(annualized_volatility, avg_daily_volume)
         ask, bid = self.pds.get_last_quote(trade_pair, processed_ms)
-        print(ask, bid)
+        # print(ask, bid)
         spread = bid - ask
         mid_price = (bid + ask) / 2
 
-        slippage_cost_percentage = intercept + (c1 * spread / mid_price) + (c2 * annualized_volatility) + (c3 * size / avg_daily_volume)
-        return slippage_cost_percentage
-
-    def get_bar_features(self, trade_pair: TradePair, processed_ms: int):
-        order_date = self.ms_to_date_string(processed_ms)
-
-        bars_df = self.get_bars_with_features(trade_pair, processed_ms)
-        # print(bars_df)
-        # order_date_dt = datetime.strptime(order_date, "%Y-%m-%d")
-        # print(order_date)
-        row_selected = bars_df[bars_df['datetime'] == order_date]
-        print(row_selected)
-        annualized_volatility = row_selected['annualized_vol'].values[0]
-        avg_daily_volume = row_selected[f'adv_last_{ValiConfig.AVERAGE_DAILY_VOLUME_LOOKBACK_DAYS}_days'].values[0]
-
-        return annualized_volatility, avg_daily_volume
+        slippage_pct = intercept + (c1 * spread / mid_price) + (c2 * annualized_volatility) + (c3 * size / avg_daily_volume)
+        return slippage_pct
 
     def calc_slippage_forex(self, trade_pair: TradePair, size: float, processed_ms: int) -> float:
         """
@@ -82,9 +69,7 @@ class PriceSlippageModel:
         slippage percentage = 0.433 * spread/mid_price + 0.335 * sqrt( TODO
         """
         annualized_volatility, avg_daily_volume = self.get_bar_features(trade_pair, processed_ms)
-        print(annualized_volatility, avg_daily_volume)
         ask, bid = self.pds.get_last_quote(trade_pair, processed_ms)
-        print(ask, bid)
         spread = bid - ask
         mid_price = (bid + ask) / 2
 
@@ -101,6 +86,7 @@ class PriceSlippageModel:
         """
         slippage values for crypto
         """
+        # TODO: assert size < 50k?
         if trade_pair in [TradePair.BTCUSD, TradePair.ETHUSD]:
             return 0.00001
         elif trade_pair in [TradePair.SOLUSD, TradePair.XRPUSD, TradePair.DOGEUSD]:
@@ -108,13 +94,23 @@ class PriceSlippageModel:
         else:
             raise ValueError(f"Unknown crypto slippage for trade pair {trade_pair.trade_pair_id}")
 
-    def get_bars_with_features(self, trade_pair, processed_ms, adv_lookback_window=10, calc_vol_window=30, trading_days_in_a_year=252):
+    def get_bar_features(self, trade_pair: TradePair, processed_ms: int) -> (float, float):
+        order_date = self.ms_to_date_string(processed_ms)
+
+        bars_df = self.get_bars_with_features(trade_pair, processed_ms)
+        row_selected = bars_df[bars_df['datetime'] == order_date]
+        print(row_selected)
+        annualized_volatility = row_selected['annualized_vol'].values[0]
+        avg_daily_volume = row_selected[f'adv_last_{ValiConfig.AVERAGE_DAILY_VOLUME_LOOKBACK_DAYS}_days'].values[0]
+
+        return annualized_volatility, avg_daily_volume
+
+    def get_bars_with_features(self, trade_pair: TradePair, processed_ms: int, adv_lookback_window: int=10, calc_vol_window: int=30, trading_days_in_a_year: int=252) -> pd.DataFrame:
         """
         fetch data for average daily volume (estimated daily volume) and annualized volatility
         """
         order_date = self.ms_to_date_string(processed_ms)
-        start_date = self.get_starting_trading_date(order_date, max(ValiConfig.AVERAGE_DAILY_VOLUME_LOOKBACK_DAYS,
-                                                           ValiConfig.ANNUALIZED_VOLATILITY_LOOKBACK_DAYS) + 1)
+        start_date = self.get_starting_trading_date(order_date, max(ValiConfig.AVERAGE_DAILY_VOLUME_LOOKBACK_DAYS, ValiConfig.ANNUALIZED_VOLATILITY_LOOKBACK_DAYS) + 1)
 
         price_info_raw = self.pds.get_candles_for_trade_pair_simple(trade_pair, start_date, order_date, timespan="day")
         aggs = []
@@ -126,14 +122,11 @@ class PriceSlippageModel:
 
         bars_pd = pd.DataFrame(aggs)
         bars_pd['datetime'] = pd.to_datetime(bars_pd['timestamp'], unit='ms').dt.strftime('%Y-%m-%d')
-        # bars_pd['weekday'] = bars_pd['datetime'].dt.weekday
-        # bars_pd = bars_pd[bars_pd['weekday'] < 5].reset_index(drop=True)  # filter out weekends
-        # bars_pd.loc[:, 'day'] = bars_pd['timestamp'].apply(lambda x: datetime.utcfromtimestamp(x / 1000).date())
         bars_pd[f'adv_last_{adv_lookback_window}_days'] = (bars_pd['volume'].rolling(window=adv_lookback_window + 1).sum() - bars_pd['volume']) / adv_lookback_window  # excluding the current day when calculating adv
         bars_pd['daily_returns'] = np.log(bars_pd["close"] / bars_pd["close"].shift(1))
         # excluding the current day when calculating vol
         bars_pd[f'rolling_avg_daily_vol_{calc_vol_window}d'] = bars_pd['daily_returns'].rolling(window=calc_vol_window, closed='left').std()
-        bars_pd[f"annualized_vol"] = bars_pd[f'rolling_avg_daily_vol_{calc_vol_window}d'] * np.sqrt(trading_days_in_a_year)
+        bars_pd["annualized_vol"] = bars_pd[f'rolling_avg_daily_vol_{calc_vol_window}d'] * np.sqrt(trading_days_in_a_year)
 
         return bars_pd
 
@@ -142,7 +135,7 @@ class PriceSlippageModel:
         get the date n trading days ago
         excludes weekends and trading holidays
         """
-        holidays = ["2025-01-01", "2025-01-09", "2025-01-20", "2024-12-25"] + self.pds.get_market_holidays()
+        holidays = ["2024-12-25", "2025-01-01", "2025-01-09", "2025-01-20"] + self.pds.get_market_holidays()  # TODO: holidays
         # print("holidays:", holidays)
         date_np = np.datetime64(date_str)
         past_date = np.busday_offset(date_np, -days_ago, roll='backward', holidays=holidays) # subtract trading days (weekends and holidays are skipped)
@@ -172,7 +165,6 @@ class PriceSlippageModel:
         if size > 300_000:
             raise ValueError("Order size must be less than $300K")
         else:
-            # TODO <1k the slippage is 0
             raise ValueError("Order size must be at least $1K")
 
     @staticmethod
