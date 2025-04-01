a9bbad5f06d88c4990315cce3866a78fd1b919d8
Jordan Bonilla <jbonilla@taoshi.io>
2025-04-01 00:36:48 -0400

    Snap return to order state at the end of interval

--- vali_objects/vali_dataclasses/perf_ledger.py
+++ vali_objects/vali_dataclasses/perf_ledger.py
@@ -1011,12 +1011,23 @@ class PerfLedgerManager(CacheController):
             accumulated_time_ms = self.inc_accumulated_time(mode, accumulated_time_ms)
 
         # Get last sliver of time for open positions and fill the void for closed positions.
+        # This also ensures return aligns with the price baked into the Order object.
         # Note - nothing changes on closed positions over time, not even fees.
+        tp_id_rtp = realtime_position_to_pop.trade_pair.trade_pair_id if realtime_position_to_pop else None
+        portfolio_correction_enabled = tp_id_rtp in tp_to_historical_positions_dense
         for tp_id in tp_ids_to_build:
            perf_ledger = perf_ledger_bundle[tp_id]
-           if perf_ledger.last_update_ms != end_time_ms:
-               assert perf_ledger.last_update_ms < end_time_ms, (perf_ledger.last_update_ms, end_time_ms)
-               perf_ledger.update_pl(tp_to_current_return[tp_id], end_time_ms, miner_hotkey, tp_to_any_open[tp_id], tp_to_current_spread_fee[tp_id], tp_to_current_carry_fee[tp_id])
+           assert perf_ledger.last_update_ms <= end_time_ms, (perf_ledger.last_update_ms, end_time_ms)
+           current_return = tp_to_current_return[tp_id]
+           if realtime_position_to_pop and tp_id_rtp in tp_ids_to_build:
+               if portfolio_correction_enabled and tp_id == TP_ID_PORTFOLIO:
+                   current_return = (tp_to_current_return[tp_id] /
+                                     tp_to_historical_positions_dense[tp_id_rtp][0].return_at_close *
+                                     realtime_position_to_pop.return_at_close)
+               elif tp_id == tp_id_rtp:
+                    current_return = realtime_position_to_pop.return_at_close
+
+           perf_ledger.update_pl(current_return, end_time_ms, miner_hotkey, tp_to_any_open[tp_id], tp_to_current_spread_fee[tp_id], tp_to_current_carry_fee[tp_id])
 
            perf_ledger.purge_old_cps()
 
@@ -1383,7 +1394,7 @@ class PerfLedgerManager(CacheController):
             # Make the plot bigger
             plt.figure(figsize=(10, 5))
             plt.plot(times, returns, color='red', label='Return')
-            plt.plot(times, returns_muled, color='blue', label='Return_Mulled')
+            #plt.plot(times, returns_muled, color='blue', label='Return_Mulled')
             plt.plot(times, mdds, color='green', label='MDD')
             # Labels
             plt.xlabel('Time')
@@ -1463,4 +1474,4 @@ if __name__ == "__main__":
     pm = PositionManager(metagraph=mmg, running_unit_tests=False, elimination_manager=elimination_manager)
     perf_ledger_manager = PerfLedgerManager(mmg, position_manager=pm, running_unit_tests=False, enable_rss=False)
     #perf_ledger_manager.update(regenerate_all_ledgers=True)
-    perf_ledger_manager.update(testing_one_hotkey='5DswH2LoRwivzv37tGvo27XJjDvFpff2fhu5T8LHsfXmFS5u')
+    perf_ledger_manager.update(testing_one_hotkey='5G1FFNUrq9UBoZjaA1Bw7JZ79EkQg9QqZpGrdNki1zvPa1e8')
