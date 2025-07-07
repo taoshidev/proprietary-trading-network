7be2ec681e36190f9faac52e7b853e0dd2dd3c12
sli-tao <sli@taoshi.io>
2025-07-07 04:19:55 -0700

    create collateral server

--- miner_config.py
+++ miner_config.py
@@ -10,7 +10,12 @@ class MinerConfig:
     AXON_NO_IP = "0.0.0.0"
 
     DASHBOARD_API_PORT = 41511
+    COLLATERAL_API_PORT = 41521
     BASE_DIR = base_directory = BASE_DIR
+    
+    # Collateral system configuration
+    COLLATERAL_ADMIN_VALIDATOR_HOTKEY_TESTNET = "5FbKk7fD33ceRDADDdpj5ULRKZMca3XW3Psoxg69iTnVjurS"
+    COLLATERAL_ADMIN_VALIDATOR_VAULT_TESTNET = "5FbsFNnBTTyKbZPWXixk5yweSjYGrUVhUbWpmC2XZQhi8uco"
 
     @staticmethod
     def get_miner_received_signals_dir() -> str:
@@ -23,3 +28,15 @@ class MinerConfig:
     @staticmethod
     def get_miner_failed_signals_dir() -> str:
         return ValiConfig.BASE_DIR + "/mining/failed_signals/"
+    
+    @staticmethod
+    def get_primary_validator_hotkey(network: str) -> str:
+        """Get collateral admin validator hotkey"""
+        if network == "test":
+            return MinerConfig.COLLATERAL_ADMIN_VALIDATOR_HOTKEY_TESTNET
+    
+    @staticmethod
+    def get_primary_validator_vault(network: str) -> str:
+        """Get collateral admin validator vault address"""
+        if network == "test":
+            return MinerConfig.COLLATERAL_ADMIN_VALIDATOR_VAULT_TESTNET
