0ffdcda803a18ed23ee62a449fe2cf38591cdcdc
Jordan Bonilla <jbonilla@taoshi.io>
2025-09-12 18:08:07 -0400

    fix tests

--- vali_objects/utils/positions_to_snap.py
+++ vali_objects/utils/positions_to_snap.py
@@ -372,15 +372,16 @@ positions_to_snap = [
     }
 
 ]
-secrets = ValiUtils.get_secrets()
-lpf = LivePriceFetcher(secrets, disable_ws=True)
-for i, position_json in enumerate(positions_to_snap):
-    # build the positions as the order edits did not propagate to position-level attributes.
-    pos = Position(**position_json)
-    pos.rebuild_position_with_updated_orders(lpf)
-    positions_to_snap[i] = pos.model_dump()
 
 if __name__ == "__main__":
+    secrets = ValiUtils.get_secrets()
+    lpf = LivePriceFetcher(secrets, disable_ws=True)
+    for i, position_json in enumerate(positions_to_snap):
+        # build the positions as the order edits did not propagate to position-level attributes.
+        pos = Position(**position_json)
+        pos.rebuild_position_with_updated_orders(lpf)
+        positions_to_snap[i] = pos.model_dump()
+
     for position_json in positions_to_snap:
         pos = Position(**position_json)
         pos.rebuild_position_with_updated_orders()
