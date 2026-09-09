14ef6d263c19fcbecc5889446ea3252c07c780b1
ward-taoshi <236940013+ward-taoshi@users.noreply.github.com>
2026-09-09 14:59:45 -0400

    initial commit

--- tests/vali_tests/test_positions.py
+++ tests/vali_tests/test_positions.py
@@ -48,6 +48,7 @@ class TestPositions(TestBase):
         open_ms=DEFAULT_OPEN_MS,
         trade_pair=DEFAULT_TRADE_PAIR,
         account_size=DEFAULT_ACCOUNT_SIZE,
+        position_type=OrderType.LONG,
     )
 
     @classmethod
